3144f38d0af762f175148b85f8c1032c4aeacce2
gloowa <gloowa@gmail.com>
2023-06-28 15:10:55 +0200

    controller

---
+++ src/main/java/net/gloowa/tests/simplemvc/service/TestService.java
@@ -0,0 +1,20 @@
+package net.gloowa.tests.simplemvc.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestService {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("/asdf")
+	public String asdf() {
+		return "asdf";
+	}
+	
+	
+}
