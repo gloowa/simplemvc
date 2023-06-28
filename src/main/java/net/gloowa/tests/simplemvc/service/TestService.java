293e4210b8004f74a8b1178ec2fa963d9cb074d9
gloowa <gloowa@gmail.com>
2023-06-28 15:25:57 +0200

    a

--- src/main/java/net/gloowa/tests/simplemvc/service/TestService.java
+++ src/main/java/net/gloowa/tests/simplemvc/service/TestService.java
@@ -3,9 +3,17 @@ package net.gloowa.tests.simplemvc.service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @RestController
 public class TestService {
 
+	private class TestBean {
+		public String name;
+		public int value;
+	}
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
@@ -13,7 +21,27 @@ public class TestService {
 	
 	@RequestMapping("/asdf")
 	public String asdf() {
-		return "asdf";
+		TestBean tb = new TestBean();
+		tb.name = "ASDF";
+		tb.value = 69;
+		
+		ObjectMapper om = new ObjectMapper();
+		
+		String result = null;
+		try {
+			result = om.writeValueAsString(tb);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			try {
+				result = om.writeValueAsString(e);
+			} catch (JsonProcessingException e1) {
+				e1.printStackTrace();
+				result = e1.getMessage();
+			}
+		} 
+		
+		return result;
+		
 	}
 	
 	
